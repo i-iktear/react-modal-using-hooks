ca0c5b29fd0ae673996f9e429149d723586b4976
Iktear Uddin <emon5aurk@gmail.com>
2020-10-24 11:41:30 +0600

    Final commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Modal from "./components/Modal";
+import { globalStyle } from "./globalStyle";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+const Button = styled.div`
+  min-width: 100px;
+  padding: 16px 32px;
+  border: none;
+  border-radius: 4px;
+  background: #299;
+  color: #fff;
+  font-size: 25px;
+  cursor: pointer;
+`;
+
+function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal((prev) => !prev);
+  };
+  return (
+    <>
+      <Container>
+        <Button onClick={openModal}>This is a Modal</Button>
+        <Modal showModal={showModal} setShowModal={setShowModal} />
+        < globalStyle />
+      </Container>
+    </>
+  );
+}
+
+export default App;
